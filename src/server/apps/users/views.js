dde66e44fe42fbfa94b985b0c7423bd41601ccf3
Jim Zhan <jim.zhan@me.com>
2017-05-30 18:35:25 +0800

    sample users resource

--- src/server/apps/users/views.js
+++ src/server/apps/users/views.js
@@ -1,20 +1,31 @@
 
 export async function list(ctx) {
-  ctx.body = 'listing users';
+  ctx.body = {
+    data: [
+      { a: { username: 'user a' } },
+      { b: { username: 'user b' } },
+      { c: { username: 'user c' } },
+    ],
+  };
 }
 
 export async function get(ctx) {
-  ctx.body = `getting user for key: ${ctx.params.key}`;
+  const data = { data: {} };
+  data[ctx.params.key] = { username: `user ${ctx.params.key}` };
+  ctx.body = data;
 }
 
 export async function create(ctx) {
-  ctx.body = 'creating new user';
+  ctx.status = 201;
+  ctx.body = { data: { abc: { username: 'user abc' } } };
 }
 
 export async function update(ctx) {
-  ctx.body = `updating user for key: ${ctx.params.key}`;
+  const data = { data: {} };
+  data[ctx.params.key] = { username: 'updated user abc' };
+  ctx.body = data;
 }
 
 export async function remove(ctx) {
-  ctx.body = `remove user for key: ${ctx.params.key}`;
+  ctx.status = 204;
 }
