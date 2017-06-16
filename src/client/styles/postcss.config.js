97c2a83d5550ef8fb93224da0f5f46cbc955fab7
Jim Zhan <jim.zhan@me.com>
2017-06-16 13:07:09 +0800

    add modern browsers supports into postcss

--- src/client/styles/postcss.config.js
+++ src/client/styles/postcss.config.js
@@ -1,4 +1,21 @@
 module.exports = () => ({
-  plugins: [
+  use: [
+    'postcss-cssnext',
+    'postcss-import',
+    'postcss-custom-properties',
+    'postcss-nesting',
+    'postcss-calc',
   ],
+  autoprefixer: {
+    browsers: [
+      'last 2 versions',
+      'ie 6-8',
+      'Firefox > 20',
+    ],
+  },
+  postcssCssnext: {
+    features: {
+      autoprefixer: true,
+    },
+  },
 });
